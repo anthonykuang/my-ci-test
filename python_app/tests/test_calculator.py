8a633d749e40f8936b0f03c10f2282614afb60c6
Anthony <orangecat38@yahoo.com.tw>
2025-08-13 16:21:14 +0800

    test

--- python_app/tests/test_calculator.py
+++ python_app/tests/test_calculator.py
@@ -10,7 +10,7 @@ class TestCalculator(unittest.TestCase):
         self.assertEqual(add(0, 0), 0)
 
     def test_subtract(self):
-        self.assertEqual(subtract(5, 2), 3)
+        self.assertEqual(subtract(6, 3), 3)
         self.assertEqual(subtract(0, 3), -3)
         self.assertEqual(subtract(7, 6), 1)
 
