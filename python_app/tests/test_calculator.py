251760ed50cac9b0cf12b55f27f06eef5dbaa873
Anthony <orangecat38@yahoo.com.tw>
2025-08-13 17:21:07 +0800

    test

--- python_app/tests/test_calculator.py
+++ python_app/tests/test_calculator.py
@@ -14,7 +14,6 @@ class TestCalculator(unittest.TestCase):
         self.assertEqual(subtract(6, 3), 3)
         self.assertEqual(subtract(0, 3), -3)
         self.assertEqual(subtract(7, 5), 2)
-        self.assertEqual(subtract(7, 5), 2)
 
 if __name__ == '__main__':
     unittest.main()
