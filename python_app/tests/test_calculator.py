d47b988324324917d6486a0405b24358f57f1b18
Anthony <orangecat38@yahoo.com.tw>
2025-08-13 15:49:04 +0800

    renew python testexample

--- python_app/tests/test_calculator.py
+++ python_app/tests/test_calculator.py
@@ -5,7 +5,7 @@ from src.calculator import add, subtract
 class TestCalculator(unittest.TestCase):
 
     def test_add(self):
-        self.assertEqual(add(3, 2), 5)
+        self.assertEqual(add(4, 1), 5)
         self.assertEqual(add(-1, -1), -2)
         self.assertEqual(add(0, 0), 0)
 
